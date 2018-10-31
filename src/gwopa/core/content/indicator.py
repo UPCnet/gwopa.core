06432b2c85354858ccccadd9323f0a52ed805616
Roberto Diaz <roberto.diaz@upcnet.es>
2018-10-31 15:53:40 +0100

    test vocabu

--- src/gwopa/core/content/indicator.py
+++ src/gwopa/core/content/indicator.py
@@ -4,6 +4,8 @@ from plone.supermodel import model
 from zope import schema
 from gwopa.core import _
 import datetime
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
 
 grok.templatedir("templates")
 
@@ -12,8 +14,28 @@ def todayValue():
     return datetime.date.today() - datetime.timedelta(1)
 
 
+indicator_kind = [
+    u"Goal",
+    u"Objective",
+    u"Outcome",
+    u"Output",
+]
+
+indicator_type = [
+    u"None",
+    u"Qualitative",
+    u"Quantitative",
+]
+
+
 class IIndicator(model.Schema):
-    """  Quantitative indicator """
+    """  indicator """
+
+    def get_vocabulary(values):
+        return SimpleVocabulary([
+            SimpleTerm(title=_(value), value=value, token=token)
+            for token, value in enumerate(values)])
+
     title = schema.TextLine(
         title=_(u"Indicator title"),
         required=True,
@@ -24,13 +46,15 @@ class IIndicator(model.Schema):
         required=True,
     )
 
-    indicator_kind = schema.TextLine(
+    indicator_kind = schema.Choice(
         title=_(u"Indicate which results chain component it is informing"),
+        vocabulary=get_vocabulary(indicator_kind),
         required=True,
     )
 
-    indicator_type = schema.TextLine(
+    indicator_type = schema.Choice(
         title=_(u"Indicator type"),
+        vocabulary=get_vocabulary(indicator_type),
         required=True,
     )
 
