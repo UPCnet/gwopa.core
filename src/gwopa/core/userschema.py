a7890ef4a4b2f0bbf0b71f172742cdb009ccbff2
Roberto Diaz <roberto.diaz@upcnet.es>
2018-11-12 12:03:44 +0100

    added goot template (aka colophon)

--- src/gwopa/core/userschema.py
+++ src/gwopa/core/userschema.py
@@ -8,6 +8,7 @@ from z3c.form import field
 from zope import schema
 from zope.component import adapts
 from zope.interface import Interface
+from gwopa.core import utils
 
 from gwopa.core import _
 from gwopa.core.interfaces import IGwopaCoreLayer
@@ -15,23 +16,59 @@ from gwopa.core.interfaces import IGwopaCoreLayer
 
 class IDemoUserSchema(model.Schema):
 
+    country = schema.Choice(
+        title=_(u"Country"),
+        description=_(u"Select country"),
+        vocabulary=utils.countries,
+        required=True,
+    )
+
+    language = schema.Choice(
+        title=_(u'Language'),
+        description=_(u"Define your language"),
+        required=True,
+        vocabulary=u"plone.app.vocabularies.SupportedContentLanguages",
+    )
+
     twitter_username = schema.TextLine(
         title=_(u'Twitter username'),
-        description=_(u'Fill in your Twitter username.'),
+        description=_(u'Your Twitter username. Could include the @. Example: @unhabitat'),
         required=False,
     )
 
     ubicacio = schema.TextLine(
         title=_(u'Location'),
-        description=_(u'help_location'),
+        description=_(u'Your location - either city and country - or in a company setting, where your office is located.'),
         required=False,
     )
 
     telefon = schema.TextLine(
         title=_(u'Phone'),
-        description=_(u'help_phone'),
+        description=_(u'Your telephone number.'),
         required=False,
     )
+
+    # OK Nombre completo del usuario (obligatorio)
+    # OK Nombre de usuario (obligatorio)
+    # OK Email de contacto del usuario (obligatorio)
+    # OK Teléfono de contacto (opcional)
+    # OK Ubicación. Lista cerrada de países (opcional)
+    # OK Idioma del usuario (a elegir entre inglés, francés o español). Por defecto inglés.
+    # Región. Lista cerrada de regiones (opcional). Definida la región, se incluyen los países asociados
+    # WOP Platform. Lista cerrada de plataformas regionales (opcional)
+    # WOP Program. Lista cerrada de programas WOP (opcional)
+    # Water Operator (partner). Lista cerrada (Obligatorio)
+    # Áreas de experiencia (lista cerrada) (opcional)
+
+    # Default fields and added ones:
+    # Fullname
+    # email
+    # User name
+    # password
+    # confirm password
+    # twitter
+    # location
+    # Phone
 
 
 class DemoUserDataSchemaAdapter(AccountPanelSchemaAdapter):
