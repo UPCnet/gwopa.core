c5ad5a9e7dcbe94a087490546259e42a42982ded
Roberto Diaz <roberto.diaz@upcnet.es>
2019-02-08 11:54:52 +0100

    added portlet

--- src/gwopa/core/browser/viewlets.py
+++ src/gwopa/core/browser/viewlets.py
@@ -3,12 +3,12 @@ from five import grok
 from plone.app.layout.viewlets.interfaces import IPortalFooter
 from plone.app.layout.viewlets.interfaces import IPortalHeader
 from zope.interface import Interface
-from Products.CMFCore.utils import getToolByName
+# from Products.CMFCore.utils import getToolByName
 
 from gwopa.core.interfaces import IGwopaCoreLayer
-from gwopa.core.content.project import IProject
-from plone.app.layout.navigation.root import getNavigationRootObject
-from Acquisition import aq_inner
+# from gwopa.core.content.project import IProject
+# from plone.app.layout.navigation.root import getNavigationRootObject
+# from Acquisition import aq_inner
 
 grok.context(Interface)
 
@@ -23,41 +23,17 @@ class viewletHeader(viewletBase):
     grok.viewletmanager(IPortalHeader)
     grok.layer(IGwopaCoreLayer)
 
-    def selected_portal_tab(self):
-        portal = getToolByName(self.context, 'portal_url').getPortalObject()
-        plone_url = getNavigationRootObject(
-            self.context, portal).absolute_url()
-        plone_url_len = len(plone_url)
-        request = self.request
+    # def selected_portal_tab(self):
+    #     portal = getToolByName(self.context, 'portal_url').getPortalObject()
+    #     plone_url = getNavigationRootObject(
+    #         self.context, portal).absolute_url()
+    #     plone_url_len = len(plone_url)
+    #     request = self.request
 
-        url = request['URL']
-        path = url[plone_url_len:]
-        path_list = path.split('/')
-        return None
-
-    def isRootFolder(self):
-        if (self.context.portal_type != 'Plone Site'):
-            return True
-        else:
-            return False
-
-    def projectPath(self):
-        if IProject.providedBy(self.context):
-            return self.context.absolute_url()
-        else:
-            portal_state = self.context.unrestrictedTraverse('@@plone_portal_state')
-            root = getNavigationRootObject(self.context, portal_state.portal())
-            physical_path = aq_inner(self.context).getPhysicalPath()
-
-            relative = physical_path[len(root.getPhysicalPath()):]
-
-            for i in range(len(relative)):
-                now = relative[:i + 1]
-                obj = aq_inner(root.restrictedTraverse(now))
-                if IProject.providedBy(obj):
-                    return obj.absolute_url()
-
-        return None
+    #     url = request['URL']
+    #     path = url[plone_url_len:]
+    #     path_list = path.split('/')
+    #     return None
 
 
 class viewletFooter(viewletBase):
---
+++ src/gwopa/core/portlets/sidenav.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from plone.app.portlets import PloneMessageFactory as _
+from plone.app.portlets.portlets import base
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implementer
+from gwopa.core.content.project import IProject
+from plone.app.layout.navigation.root import getNavigationRootObject
+from Acquisition import aq_inner
+
+
+class ISideNavPortlet(IPortletDataProvider):
+    """ GWOPA SIDE NAVIGATION """
+
+
+@implementer(ISideNavPortlet)
+class Assignment(base.Assignment):
+
+    title = _(u'gwopa_menu', default=u'GWOPA Menu')
+
+
+class Renderer(base.Renderer):
+
+    render = ViewPageTemplateFile('sidenav.pt')
+
+    def isRootFolder(self):
+        if (self.context.portal_type != 'Plone Site'):
+            return True
+        else:
+            return False
+
+    def projectPath(self):
+        if IProject.providedBy(self.context):
+            return self.context.absolute_url()
+        else:
+            portal_state = self.context.unrestrictedTraverse('@@plone_portal_state')
+            root = getNavigationRootObject(self.context, portal_state.portal())
+            physical_path = aq_inner(self.context).getPhysicalPath()
+
+            relative = physical_path[len(root.getPhysicalPath()):]
+
+            for i in range(len(relative)):
+                now = relative[:i + 1]
+                obj = aq_inner(root.restrictedTraverse(now))
+                if IProject.providedBy(obj):
+                    return obj.absolute_url()
+
+        return None
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
