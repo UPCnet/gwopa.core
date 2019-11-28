6d1dd95ef69c2ba6e85a5de9a53aae9e6f3530ab
pilar.marinas <pilar.marinas@upcnet.es>
2019-11-28 09:40:08 +0100

    add logger in hook project added

--- src/gwopa/core/hooks.py
+++ src/gwopa/core/hooks.py
@@ -163,6 +163,8 @@ def projectAdded(content, event):
             transaction.commit()
             logger.info('Create working area {} in project {} id {}'.format(data.Title, content.title, content.id))
 
+    logger.info('Finish create working areas in project {} id {}'.format(content.title, content.id))
+
     partners = content.partners
     if partners:
         for partner in partners:
@@ -439,7 +441,7 @@ def improvementAreaAdded(content, event):
         container=content)
 
     transaction.commit()
-    logger.info('Create folder events in content {} id {}'.format(content.title, content.id))
+    logger.info('Create folder events in working area {} id {}'.format(content.title, content.id))
 
     api.content.create(
         type='Folder',
@@ -448,7 +450,7 @@ def improvementAreaAdded(content, event):
         container=content)
 
     transaction.commit()
-    logger.info('Create folder files in content {} id {}'.format(content.title, content.id))
+    logger.info('Create folder files in working area {} id {}'.format(content.title, content.id))
 
     api.content.create(
         type='Folder',
@@ -457,7 +459,7 @@ def improvementAreaAdded(content, event):
         container=content)
 
     transaction.commit()
-    logger.info('Create folder topics in content {} id {}'.format(content.title, content.id))
+    logger.info('Create folder topics in working area {} id {}'.format(content.title, content.id))
 
     obj = api.content.create(
         type='OutcomeCC',
@@ -466,7 +468,7 @@ def improvementAreaAdded(content, event):
         container=content)
 
     transaction.commit()
-    logger.info('Create OutcomeCC in content {} id {}'.format(content.title, content.id))
+    logger.info('Create OutcomeCC in working area {} id {}'.format(content.title, content.id))
 
     annotations = IAnnotations(obj)
     for x in range(0, 11):  # Create 10 annotations
@@ -538,7 +540,7 @@ def improvementAreaAdded(content, event):
         KEY = "GWOPA_TARGET_YEAR_" + str(x + 1)
         annotations[KEY] = data
 
-        logger.info('Create annotations in content {} id {}'.format(content.title, content.id))
+    logger.info('Create annotations in content {} id {}'.format(content.title, content.id))
 
 
 #@grok.subscribe(IImprovementArea, IObjectModifiedEvent)
@@ -564,9 +566,10 @@ def OutcomeCCAdded(content, event):
             title_es=item.title_es,
             title_fr=item.title_fr,
             container=content)
+        logger.info('Create OutcomeCCS {} in project {} id {}'.format(item.Title, content.__parent__.__parent__.title, content.__parent__.__parent__.id))
         transaction.commit()
-        logger.info('Create OutcomeCCS {} in project {} id {}'.format(item.Title, content.title, content.id))
 
+    logger.info('Finish create OutcomeCCS in project {} id {}'.format(content.__parent__.__parent__.title, content.__parent__.__parent__.id))
 
 @grok.subscribe(IConfigurationChangedEvent)
 def updateCustomLangCookie(event):
