037524ba137152aa0679308af1cc03eb27d7dce8
Iago López Fernández <iago.lopez@upcnet.es>
2019-12-04 13:52:57 +0100

    Añadir selector del proyect year antes de acceder al preview del report

--- src/gwopa/core/browser/reporting.py
+++ src/gwopa/core/browser/reporting.py
@@ -3,6 +3,10 @@ from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from operator import itemgetter
+
+from gwopa.core import _
+
 
 class reportingView(BrowserView):
     """ Shows all the reporting options associated to one project
@@ -24,3 +28,23 @@ class reportingView(BrowserView):
 
     def projectURL(self):
         return self.context.absolute_url()
+
+    def getItems(self):
+        """ Returns all the project years of the planning """
+        items = len(self.context.gwopa_year_phases)
+        results = []
+        total = 0
+
+        while total != items:
+            if total == 0:
+                url = self.context.absolute_url_path() + '/reportPreview/'
+            else:
+                url = self.context.absolute_url_path() + '/reportPreview/' + str(total + 1)
+            results.append(dict(
+                title=_(u"Project year"),
+                year=str(total + 1),
+                url=url,
+                alt=_(u"Show report preview of year ") + str(total + 1)))
+            total = total + 1
+
+        return sorted(results, key=itemgetter('title'), reverse=False)
