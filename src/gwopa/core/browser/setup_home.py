7cbf1ab54a8acd249c6b91a4f46c71e2c6216b00
Iago López Fernández <iago.lopez@upcnet.es>
2020-02-06 12:07:13 +0100

    Añadir nuevos valores de unidad

--- src/gwopa/core/browser/setup_home.py
+++ src/gwopa/core/browser/setup_home.py
@@ -238,7 +238,7 @@ class setup(grok.View):
             container=portal.config,
             safe_id=False)
         settingspage.currency = 'USD-US Dollar-$\r\nEUR-Euro-€\r\nGBP-British Pound-£\r\nAUD-Australian Dollar-$\r\nCAD-Canadian Dollar-$'
-        settingspage.measuring_unit = 'liters\nm3\npeople\nothers'
+        settingspage.measuring_unit = 'liters\nm3\npeople\nconnections\nhouseholds\nkilometers\nstaff by population\nstaff by connection\ncost recovery ratio\nworkplace accidents\nworker satisfaction\nhours\ndays\npercentage\nother'
         settingspage.measuring_frequency = 'Annually,1\nBiannually,2\nQuarterly,4'
         settingspage.degree_changes = '-2 Very negative\n-1 Negative\n0 No change\n1 Positive\n2 Very positive'
         settingspage.contributed_project = '0 No contribution\n1 Fair contribution\n2 Very high contribution'
@@ -247,7 +247,7 @@ class setup(grok.View):
         settingspage.organization_roles = 'Broker (matchmaker, facilitator)\nTechnical Support\nPolitical Support'
 
         settingspage.currency_es = 'USD-Dólar estadounidense-$\r\nEUR-Euro-€\r\nGBP-Libra británica-£\r\nAUD-Dólar australiano-$\r\nCAD-Dolar canadiense-$'
-        settingspage.measuring_unit_es = 'litros\nm3\npersonas\notros'
+        settingspage.measuring_unit_es = 'litros\nm3\npersonas\nconexiones\nhogares\nkilómetros\npersonal por población\npersonal por conexión\nrelación de recuperación de costos\naccidentes laborales\nsatisfacción del trabajador\nhoras\ndias\nporcentaje\notro'
         settingspage.measuring_frequency_es = 'Anualmente,1\nBianualmente,2\nTrimestralmente,4'
         settingspage.degree_changes_es = '-2 Muy negativo\n-1 Negativo\n0 Sin cambios\n1 Positivo\n2 Muy positivo'
         settingspage.contributed_project_es = '0 Sin contribución\n1 Contribución justa\n2 Contribución muy alta'
@@ -257,7 +257,7 @@ class setup(grok.View):
 
         # TODO: Falta traducir los valores al Frances
         settingspage.currency_fr = 'USD-US Dollar-$\r\nEUR-Euro-€\r\nGBP-British Pound-£\r\nAUD-Australian Dollar-$\r\nCAD-Canadian Dollar-$'
-        settingspage.measuring_unit_fr = 'liters\nm3\npeople\nothers'
+        settingspage.measuring_unit_fr = 'liters\nm3\npeople\nconnections\nhouseholds\nkilometers\nstaff by population\nstaff by connection\ncost recovery ratio\nworkplace accidents\nworker satisfaction\nhours\ndays\npercentage\nother'
         settingspage.measuring_frequency_fr = 'Annually,1\nBiannually,2\nQuarterly,4'
         settingspage.degree_changes_fr = '-2 Very negative\n-1 Negative\n0 No change\n1 Positive\n2 Very positive'
         settingspage.contributed_project_fr = '0 No contribution\n1 Fair contribution\n2 Very high contribution'
@@ -269,7 +269,6 @@ class setup(grok.View):
         updateDictsSetting(settingspage)
 
         portal = api.portal.get()
-
 
         # Create base Outcome CC selectable Values
         tool_ts = getToolByName(self, 'translation_service')
