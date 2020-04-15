0adf8b7ad7bbe3b8905432bcffc62214eaf2da6c
Iago López Fernández <iago.lopez@upcnet.es>
2020-04-15 08:26:06 +0200

    Traducciones

---
+++ src/gwopa/core/locales/translations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+msgfmt -o es/LC_MESSAGES/gwopa.mo  es/LC_MESSAGES/gwopa.po
+msgfmt -o en/LC_MESSAGES/gwopa.mo  en/LC_MESSAGES/gwopa.po
+msgfmt -o fr/LC_MESSAGES/gwopa.mo  fr/LC_MESSAGES/gwopa.po
