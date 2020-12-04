621a97b1206b5a7d0128e0067ac15d93c437c094
Iago López Fernández <iago.lopez@upcnet.es>
2020-12-04 12:52:36 +0100

    Etapas monitoring

--- src/gwopa/core/browser/monitoring.py
+++ src/gwopa/core/browser/monitoring.py
@@ -536,7 +536,7 @@ class monitoringView(BrowserView):
             end + """]}, "time": false, "today": false, "clear": true}"""
         return value
 
-    def getCurrentStage(self):
+    def getCurrentStage(self, title):
         """ Returns all the stages for each Improvement Areas in a Project """
         items = api.content.find(
             portal_type=['OutcomeCC'],
@@ -544,26 +544,35 @@ class monitoringView(BrowserView):
         results = []
         KEY = "GWOPA_TARGET_YEAR_" + str(self.year)
         for item in items:
-            annotations = IAnnotations(item.getObject())
-            stage = annotations[KEY]['generic'][0]['stage']
-            if stage:
-                for i in range(1, 5):
-                    if i < int(stage):
-                        state = "past"
-                    elif i == int(stage):
-                        state = "current"
-                    else:
-                        state = "future"
-                    results.append(dict(id="stage-" + str(i),
-                                        title="Stage " + str(i),
-                                        description="Stage " + str(i) + " Description",
-                                        state=state))
-            else:
-                for i in range(1, 5):
-                    results.append(dict(id="stage-" + str(i),
-                                        title="Stage " + str(i),
-                                        description="Stage " + str(i) + " Description",
-                                        state="future"))
+            if item.getObject().aq_parent.Title() == title:
+                annotations = IAnnotations(item.getObject())
+                stage = annotations[KEY]['generic'][0]['stage']
+                if stage:
+                    for i in range(1, 5):
+                        if i < int(stage):
+                            state = "past"
+                        elif i == int(stage):
+                            state = "current"
+                        else:
+                            state = "future"
+                        results.append(dict(id="stage-" + str(i),
+                                            title="Stage " + str(i),
+                                            description="Stage " + str(i) + " Description",
+                                            state=state))
+                else:
+                    for i in range(1, 5):
+                        results.append(dict(id="stage-" + str(i),
+                                            title="Stage " + str(i),
+                                            description="Stage " + str(i) + " Description",
+                                            state="future"))
+
+                return results[0:4]
+
+        for i in range(1, 5):
+            results.append(dict(id="stage-" + str(i),
+                                title="Stage " + str(i),
+                                description="Stage " + str(i) + " Description",
+                                state="future"))
 
         return results[0:4]
 
